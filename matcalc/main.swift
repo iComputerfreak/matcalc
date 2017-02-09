8f94792a3514c8cfc55e96aca3c29c42b269c4ed
Jonas Frey <jonasfrey@scc-wkit-clxa-15-143.scc.kit.edu>
2017-02-09 15:15:05 +0100

    Initial Commit

---
+++ matcalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  matcalc
+//
+//  Created by Jonas Frey on 09.02.17.
+//  Copyright © 2017 Jonas Frey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
